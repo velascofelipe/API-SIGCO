4269450c5778944a2e555e09ffe1c4cff130cc20
Felipe Velasco <smyxone@gmail.com>
2024-01-22 12:20:34 -0300

    cambios en estructura

--- src/main/java/io/sigco/contactapi/constant/Constant.java
+++ src/main/java/io/sigco/contactapi/constant/Constant.java
@@ -5,7 +5,7 @@ public class Constant {
 
 
 
-    // Directorio donde se almacenarán las fotos (se utiliza el directorio de descargas del usuario)
+    // Directorio donde se almacenarán las fotoss (se utiliza el directorio de descargas del usuario)
     public static final String PHOTO_DIRECTORY = System.getProperty("user.home") + "/Downloads/uploads/";
 
     // Cabecera HTTP utilizada para indicar el tipo de solicitud (comúnmente asociada con AJAX)
